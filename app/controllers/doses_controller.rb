398daf560d95e4a347142a4cce9920796aa6398f
Adrien du Peloux <adrien.dupeloux@edhec.com>
2017-10-02 16:10:26 +0200

    ENFIN

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,28 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(safe_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy!
+  end
+
+  private
+
+  def safe_params
+    params.require(:dose).permit(:ingredient_id, :description)
+  end
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,7 +1,7 @@
 class Dose < ApplicationRecord
   validates :description, presence: true
   validates :cocktail, presence: true, uniqueness: { scope: :ingredient_id }
-  validates :ingredient, presence: true
+  validates :ingredient_id, presence: true, numericality: { only_integer: true }
 
   belongs_to :ingredient
   belongs_to :cocktail
